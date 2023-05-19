1faaf15b672fa18d998a5737f5dabb3304b7c1de
Tomek <tomsolga90@gmail.com>
2023-05-19 15:07:19 +0200

    config twin macro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { TopSection } from '../containers/TopSection';
+import tw from 'twin.macro';
+
+const AppContainer = tw.div`
+  flex
+  w-full
+  h-full
+  bg-indigo-300
+`;
+
 export default function Home() {
   return (
-    <main>
-      ---
-    </main>
+    <AppContainer>
+      <TopSection />
+    </AppContainer>
   )
 }
