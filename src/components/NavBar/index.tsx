5dd60343ffdf95957cec2925e41535cd57fe8429
Tomek <tomsolga90@gmail.com>
2023-06-05 23:59:15 +0200

    add r-b-m dev deps, implementation of r-b-m

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,6 +5,7 @@ import { Logo } from '../Logo';
 import { Link } from 'react-scroll';
 import { deviceSize } from '../responsive';
 import { useMediaQuery } from 'react-responsive';
+import { slide as Menu } from 'react-burger-menu';
 
 const NavBarContainer = styled.div`
   ${tw`flex items-center self-center h-20 pt-4 pl-6 pr-6 border-b-2 border-gray-200 border-opacity-50 `};
@@ -58,7 +59,8 @@ export function NavBar() {
   return (
     <NavBarContainer>
       <Logo />
-      
+      {isMobile && <Menu right >{ navBarItems }</Menu>}
+      {!isMobile && navBarItems}
     </NavBarContainer>
   )
 }
